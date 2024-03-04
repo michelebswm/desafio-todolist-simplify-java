4d98615973d3e1a46732a517a9cdb9bad4d0bdb6
michelebswm <michele.fenix.17@gmail.com>
2024-03-04 07:19:12 -0300

    fix: insere o campo done

--- src/main/java/com/michelewm/desafiotodolist/dtos/TaskDTO.java
+++ src/main/java/com/michelewm/desafiotodolist/dtos/TaskDTO.java
@@ -3,6 +3,6 @@ package com.michelewm.desafiotodolist.dtos;
 import com.michelewm.desafiotodolist.domain.enums.Priority;
 import com.michelewm.desafiotodolist.domain.enums.TaskStatus;
 
-public record TaskDTO(String title, String description, Priority priority, TaskStatus taskStatus ) {
+public record TaskDTO(String title, String description, Priority priority, TaskStatus taskStatus, Boolean done ) {
 
 }
