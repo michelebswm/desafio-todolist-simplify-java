95116b771287f3ceec58849cd68e6e4cb856a047
michelebswm <michele.fenix.17@gmail.com>
2024-03-05 07:25:43 -0300

    feat: adiciona funcao para converter texto recebido da url em utf8

---
+++ src/main/java/com/michelewm/desafiotodolist/controllers/util/URL.java
@@ -0,0 +1,12 @@
+package com.michelewm.desafiotodolist.controllers.util;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
+public class URL {
+
+    public static String decodeParam(String text){
+        return URLDecoder.decode(text, StandardCharsets.UTF_8);
+    }
+
+}
