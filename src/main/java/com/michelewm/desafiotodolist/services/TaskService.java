8a8c084ef97ee67d9c4aaf024f0663903235d5dc
michelebswm <michele.fenix.17@gmail.com>
2024-03-05 19:55:30 -0300

    fix: mudifica a injeção de dependencia e cria um Sort ao selecionar todas as tasks

--- src/main/java/com/michelewm/desafiotodolist/services/TaskService.java
+++ src/main/java/com/michelewm/desafiotodolist/services/TaskService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.michelewm.desafiotodolist.domain.Task;
@@ -17,56 +18,66 @@ import com.michelewm.desafiotodolist.services.exceptions.ValidationTaskException
 @Service
 public class TaskService {
 
-    @Autowired
     private TaskRepository repository;
 
-    public List<Task>  getAllTasks() { return repository.findAll(); }
-    
-    public Task getTaskById(Long id) { 
-        Optional<Task> optionalTask = repository.findById(id);
-        return optionalTask.orElseThrow(()-> new ResourceNotFoundException(id));
+    public TaskService(TaskRepository repository) {
+        this.repository = repository;
     }
 
-    public Task insert(TaskDTO taskData){
+    public List<Task> getAllTasks() {
+        Sort sort = Sort.by("priority").descending().and(Sort.by("title").ascending());
+        return repository.findAll(sort);
+    }
+
+    public Task getTaskById(Long id) {
+        Optional<Task> optionalTask = repository.findById(id);
+        return optionalTask.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Task insert(TaskDTO taskData) {
         validateTasks(taskData.taskStatus(), taskData.done());
         Task newTask = new Task(taskData);
         return repository.save(newTask);
     }
 
-    public Task update(Long id, TaskDTO taskData){
+    public Task update(Long id, TaskDTO taskData) {
         Task task = getTaskById(id);
-        
+
         validateTasks(taskData.taskStatus(), taskData.done());
-        if (!task.getTitle().isEmpty())  task.setTitle(taskData.title());
-        if (!task.getDescription().isEmpty()) task.setDescription(taskData.description());
-        if (!(task.getPriority() == null)) task.setPriority(taskData.priority());  
-        if (!(task.getTaskStatus() == null)) task.setTaskStatus(taskData.taskStatus());
-        task.setDone(taskData.done());  
-        return repository.save(task) ;
+        if (!task.getTitle().isEmpty())
+            task.setTitle(taskData.title());
+        if (!task.getDescription().isEmpty())
+            task.setDescription(taskData.description());
+        if (!(task.getPriority() == null))
+            task.setPriority(taskData.priority());
+        if (!(task.getTaskStatus() == null))
+            task.setTaskStatus(taskData.taskStatus());
+        task.setDone(taskData.done());
+        return repository.save(task);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         Task task = getTaskById(id);
         repository.delete(task);
     }
-    
-    public List<Task> findByIsDone(boolean done){
+
+    public List<Task> findByIsDone(boolean done) {
         return repository.findByIsDone(done);
     }
 
-    public List<Task> findByTitleContainingIgnoreCase(String title){
-        return  repository.findByTitleContainingIgnoreCase(title);
+    public List<Task> findByTitleContainingIgnoreCase(String title) {
+        return repository.findByTitleContainingIgnoreCase(title);
     }
 
     public List<Task> findByPriority(Priority priority) {
         return repository.findByPriority(priority);
     }
 
-    public void validateTasks(TaskStatus taskStatus, Boolean done){
-        if (taskStatus.getCode() == 2 && done == false){
+    public void validateTasks(TaskStatus taskStatus, Boolean done) {
+        if (taskStatus.getCode() == 2 && done == false) {
             throw new ValidationTaskException("Status DONE precisa ser marcado como concluído");
         }
-        if (done == true && taskStatus.getCode() != 2){
+        if (done == true && taskStatus.getCode() != 2) {
             throw new ValidationTaskException("O status CONCLUÍDO só pode ser usado para tarefas finalizadas");
         }
     }
